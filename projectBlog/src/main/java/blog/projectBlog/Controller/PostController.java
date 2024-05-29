bf0cdc7268b07f457af3ccbc5bafdd7c8bfaf487
Ezequiel <ezequielalves1021@gmail.com>
2024-05-29 17:35:46 -0300

    Connecting Controller to Service to Repository

--- projectBlog/src/main/java/blog/projectBlog/Controller/PostController.java
+++ projectBlog/src/main/java/blog/projectBlog/Controller/PostController.java
@@ -1,9 +1,14 @@
 package blog.projectBlog.Controller;
 
+import blog.projectBlog.Service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/Posts")
 public class PostController {
+
+    @Autowired
+    private PostService service;
 }
--- projectBlog/src/main/java/blog/projectBlog/Service/PostService.java
+++ projectBlog/src/main/java/blog/projectBlog/Service/PostService.java
@@ -1,8 +1,12 @@
 package blog.projectBlog.Service;
 
+import blog.projectBlog.Repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PostService {
 
+    @Autowired
+    private PostRepository repository;
 }
