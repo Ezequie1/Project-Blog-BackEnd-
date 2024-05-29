6da65c6c1cc036bcdbf3d4c0aca1c7673d2b6a9c
Ezequiel <ezequielalves1021@gmail.com>
2024-05-29 17:28:43 -0300

    Create PostRepository

---
+++ projectBlog/src/main/java/blog/projectBlog/Repository/PostRepository.java
@@ -0,0 +1,9 @@
+package blog.projectBlog.Repository;
+
+import blog.projectBlog.Model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
