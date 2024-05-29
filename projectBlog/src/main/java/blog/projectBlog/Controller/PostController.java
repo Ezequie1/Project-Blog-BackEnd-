dd87775cdf3fb21edcc30639a956f01ae1f68377
Ezequiel <ezequielalves1021@gmail.com>
2024-05-29 17:32:29 -0300

    Creating PostController to receive all requests

---
+++ projectBlog/src/main/java/blog/projectBlog/Controller/PostController.java
@@ -0,0 +1,9 @@
+package blog.projectBlog.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Posts")
+public class PostController {
+}
