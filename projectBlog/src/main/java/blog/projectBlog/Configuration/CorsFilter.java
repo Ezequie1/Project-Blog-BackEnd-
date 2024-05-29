847b224b613e007381cb40035a9012d7d07ff2d2
Ezequiel <ezequielalves1021@gmail.com>
2024-05-29 19:13:47 -0300

    Implementing logics for the CORS not block requests when aplication stayer in PROD

---
+++ projectBlog/src/main/java/blog/projectBlog/Configuration/CorsFilter.java
@@ -0,0 +1,23 @@
+package blog.projectBlog.Configuration;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class CorsFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response,
+                                    final FilterChain filterChain) throws ServletException, IOException{
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, HEAD");
+        response.addHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+        filterChain.doFilter(request, response);
+    }
+}
