763b310616f74796dcccb87049516823a9bd8eaa
Ezequiel <ezequielalves1021@gmail.com>
2024-05-29 18:16:21 -0300

    Creating first controller method and add logic to get data in repository, added Swagger to view

--- projectBlog/src/main/java/blog/projectBlog/Controller/PostController.java
+++ projectBlog/src/main/java/blog/projectBlog/Controller/PostController.java
@@ -1,9 +1,14 @@
 package blog.projectBlog.Controller;
 
+import blog.projectBlog.Model.Post;
 import blog.projectBlog.Service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/Posts")
@@ -11,4 +16,9 @@ public class PostController {
 
     @Autowired
     private PostService service;
+
+    @GetMapping
+    public ResponseEntity<List<Post>> getPosts(){
+        return ResponseEntity.status(200).body(service.getAll());
+    }
 }
--- projectBlog/src/main/java/blog/projectBlog/Service/PostService.java
+++ projectBlog/src/main/java/blog/projectBlog/Service/PostService.java
@@ -1,12 +1,19 @@
 package blog.projectBlog.Service;
 
+import blog.projectBlog.Model.Post;
 import blog.projectBlog.Repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PostService {
 
     @Autowired
     private PostRepository repository;
+
+    public List<Post> getAll(){
+        return repository.findAll();
+    }
 }
