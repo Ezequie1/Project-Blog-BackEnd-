51290ef1885abe8e68ac223642d5a98d14e7f36d
Ezequiel <ezequielalves1021@gmail.com>
2024-06-16 13:03:45 -0300

    Updating CORS filter

--- projectBlog/src/main/java/blog/projectBlog/Configuration/CorsFilter.java
+++ projectBlog/src/main/java/blog/projectBlog/Configuration/CorsFilter.java
@@ -18,6 +18,7 @@ public class CorsFilter extends OncePerRequestFilter {
         response.addHeader("Access-Control-Allow-Origin", "*");
         response.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, HEAD");
         response.addHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+        response.addHeader("Access-Control-Allow-Credentials", "true");
         filterChain.doFilter(request, response);
     }
 }
